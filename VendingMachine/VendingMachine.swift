570a8d2b499d666aba3308fe53a569be5dc86017
Jordan Leahy <jordanwilliamleahy@gmail.com>
2018-06-10 16:35:23 -0400

    Added properties and func to VendingMachine Protocol
    Added inventory var
    Added inventory var
    Added init method
    Added vend func
    Added deposit func

--- VendingMachine/VendingMachine.swift
+++ VendingMachine/VendingMachine.swift
@@ -23,8 +23,23 @@ enum VendingSelection {
     case gum
 }
 
-//Pass in
+protocol VendingItem {
+    var price: Double { get }
+    var quantity: Int { get set }
+}
+
+//Each VendingMachine should have a selection: and this should be an array that we have defined in our VendingMaching enum
+//VendingMachine also needs an inventory which is a dictionary with a keys of Vending Selection and Vendingitem as value
 protocol VendingMachine {
     var selection: [VendingSelection] { get }
-    var inventory: [VendingSelection: VendingItem]
+    var inventory: [VendingSelection: VendingItem] { get set }
+    var amountDeposited: Double { get set }
+    
+    //A vending machine needs to set itself up from some inventory source.
+    init(inventory: [VendingSelection: VendingItem])
+    
+    //A vending machine needs to vend items and can lead to many errors
+    func vend(_ selection: VendingSelection, _ quantity: Int) throws
+    func deposit(_ amount: Double)
 }
+
